b7c8f56ffb30ea224b958aef604a4741bc593279
ankitmrmishra <ankitmrmishra1118@gmail.com>
2024-09-18 03:51:50 +0530

    first push 4a.m.

---
+++ components/Phone.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { HTMLAttributes, ReactNode } from "react";
+
+interface Phoneprops extends HTMLAttributes<HTMLDivElement> {
+  imgsrc: string;
+  dark?: boolean;
+}
+
+const Phone = ({ className, imgsrc, dark = false, ...props }: Phoneprops) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <img
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        alt=""
+        className="pointer-events-none z-50 select-none"
+      />
+
+      <div className="absolute -z-10 inset-0">
+        <img src={imgsrc} alt="kjef" className="object-cover" />
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
