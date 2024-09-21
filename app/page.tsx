8241afd95b510dd13c181286dde150ad9315a90a
ankitmrmishra <ankitmrmishra1118@gmail.com>
2024-09-21 10:16:45 +0530

    prod fail fix unused vars

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Reviews } from "@/components/Reviews";
 
 export default function Home() {
   return (
-    <div className="">
+    <div>
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
